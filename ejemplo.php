39e91b92e12b6780b2e445c8bad4f8c556a64067
benjamin serrano gomez-landero <tarea5despliegue@gmail.com>
2018-05-10 18:25:35 +0200

    ejemplo.php comentado

--- ejemplo.php
+++ ejemplo.php
@@ -1,10 +1,36 @@
 <?php
+/**
+* ejemplo.php 
+*
+* Este archivo contiene una función para multiplicar 3 números y una clase que hará uso
+* de dicha función.
+*
+*/
 
+/**
+* multiplicarTresNumeros
+*
+* Función que multiplica tres números
+*
+* @param $v1 Primer número que se usará en la multiplicación.
+* @param $v2 Segundo número que se empleará en la multiplicación.
+* @param $v3 Tercer número que se utilizará en la multiplicación.
+* @return resultado de la multiplicación.
+*/
 function multiplicarTresNumeros ($v1, $v2, $v3)
 {
 	return $v1*$v2*$v3;
 }
 
+/**
+* Clase Ejemplo
+*
+* Clase que se empleará para realizar la multiplicación de los tres 
+* atributos o propiedades de la clase haciendo uso de la función 
+* multiplicarTresNumeros
+*
+* @author Benjamín Serrano Gómez-Landero
+*/
 class Ejemplo {
 
 	var $a;
